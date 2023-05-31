6751c0e51f02c6cfaba53656449cf725311c1c11
roninvictus <A00254959@mycambrian.ca>
2023-05-30 20:26:22 -0400

    first commit

---
+++ NumbersBetweenApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace NumbersBetweenApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //method for the code that will be executed when the button is clicked.
+        private void ShowNumbersButton_Clicked(object sender, EventArgs e)
+        {
+            /*to convert the text entered in the startNumberEntry Entry control
+            into an integer and storing it in the startNumber variable.*/
+            int startNumber = Convert.ToInt32(startNumberEntry.Text);
+
+            /*converts the text entered in the endNumberEntry Entry control into 
+              integer and stores it in the endNumber variable.*/
+            int endNumber = Convert.ToInt32(endNumberEntry.Text);
+
+                                //converts the specified value to a 32-bit signed integer.
+            int incrementValue = Convert.ToInt32(incrementEntry.Text);
+
+            if (startNumber > endNumber)
+            {
+                DisplayAlert("Error", "Start number should be less than end number.", "OK");
+                return;
+            }
+
+            for (int i = startNumber; i <= endNumber; i += incrementValue)
+            {
+                numberLabel.Text += $"{i} ";
+            }
+        }
+    }
+}
---
+++ NumbersBetweenApp/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("NumbersBetweenApp.MainApplication, NumbersBetweenApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc647413c466d34e6cd4.MainApplication.class, crc647413c466d34e6cd4.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
